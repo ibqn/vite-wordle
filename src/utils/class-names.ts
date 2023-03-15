223cc76e3ebd186f36292bbb68c2e0716bfed846
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-03-15 21:00:36 +0100

    improve type definition of classNames

--- src/utils/class-names.ts
+++ src/utils/class-names.ts
@@ -1,4 +1,8 @@
-export type ClassName = string | boolean | undefined
+import { type ComponentProps } from 'preact'
+
+export type ClassName =
+  | boolean
+  | Pick<ComponentProps<'div'>, 'className'>['className']
 
 export const classNames = (...classes: ClassName[]) =>
   classes.filter(Boolean).join(' ')
